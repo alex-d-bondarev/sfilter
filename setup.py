29a4e705036a497ea0d18b590bd92f8922f04d8a
Alex <alex.d.bondarev@gmail.com>
2021-10-24 12:48:33 +0100

    create branch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name='sfilter',
-    version='0.0.2',
+    version='0.0.3',
     python_requires=">=3.9",
     author='Sasha Bondarev (Oleksandr)',
     author_email='alex.d.bondarev@gmail.com',
