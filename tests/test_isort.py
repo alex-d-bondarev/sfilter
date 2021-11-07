8156f5bf589d7ade0091f09af9f7dd5338dbf34a
Alex <alex.d.bondarev@gmail.com>
2021-11-07 11:11:33 +0000

    update isort call

---
+++ tests/test_isort.py
@@ -0,0 +1,20 @@
+import pytest  # noqa
+
+from src.sfilter.tools.isort import run_isort
+from tests.fixtures import create_temp_file  # noqa
+
+
+@pytest.mark.parametrize("create_temp_file", [{
+    "file_name": "temp_test_isort.py",
+    "file_content": "import pathlib\nimport os"
+}], indirect=True)
+def test_isort(create_temp_file):
+    """Test that flake8 is launched"""
+    expected = "import os\nimport pathlib\n"
+    actual = ""
+    run_isort(create_temp_file)
+
+    for line in open(create_temp_file):
+        actual += line
+
+    assert actual == expected
