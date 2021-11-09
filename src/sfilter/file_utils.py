ebc146eba0b2a8489b2ecdba3fe3e7c75d9f0435
Alex <alex.d.bondarev@gmail.com>
2021-11-09 21:58:32 +0000

    refactor FileUtils

--- src/sfilter/file_utils.py
+++ src/sfilter/file_utils.py
@@ -1,36 +1,18 @@
-import os
-import pathlib
+from pathlib import Path
 
 
 class FileUtils(object):
     """File utilities"""
 
     def __init__(self, name: str):
-        self.file_name = name
-        self.path_to_file = self._get_file_path()
-
-    def _get_file_path(self) -> str:
-        path = os.path.join(pathlib.Path().parent.resolve(), self.file_name)
-        if os.path.isfile(path):
-            return path
-        else:
-            path = os.path.join(pathlib.Path().parent.parent.resolve(), self.file_name)
-            if os.path.isfile(path):
-                return path
-            else:
-                raise FileNotFoundError(self.file_name)
+        self.path_to_file = next(Path().parent.parent.glob(name))
 
     def get_file_content(self) -> str:
         """Get content of the file that was given in constructor
         :return: file content as text
         """
-        file_content = ""
-
-        for line in open(self.path_to_file):
-            file_content += line
-
-        return file_content
+        return self.path_to_file.read_text()
 
     def delete_file(self):
         """Self evident"""
-        os.remove(self.path_to_file)
+        self.path_to_file.unlink(missing_ok=True)
