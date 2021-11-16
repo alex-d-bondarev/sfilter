f82cea217452e17fa4b232bb31c1f0631ca65564
Alex <alex.d.bondarev@gmail.com>
2021-11-16 22:25:40 +0000

    remove .properties file

--- tests/sfilter/test_properties.py
+++ tests/sfilter/test_properties.py
@@ -5,6 +5,7 @@ from src.sfilter.setup_handler import SetUpHandler
 def test_sfilter_section_is_present():
     assert SetUpHandler().has_section("sfilter")
 
+
 def test_setup_writes_without_changes():
     content_before = find_file("setup.cfg").get_content()
     SetUpHandler().save()
